a012aa70318c054a15932ce850e25f513d35b38e
G1Joshi <joshijeevan98@gmail.com>
2021-04-12 23:15:20 +0530

    add prime number in range

---
+++ Print All Primes Till N.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        for (int i = a; i <= b; i++) {
+            int flag = 0;
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0)
+                System.out.println(i);
+        }
+    }
+}
