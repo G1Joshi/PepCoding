3ff91be3f52521c7ebb71151e5c9317f317d1ce2
G1Joshi <joshijeevan98@gmail.com>
2021-04-12 23:09:15 +0530

    add marks grading

---
+++ Grading System.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int marks = scan.nextInt();
+        if (marks > 90)
+            System.out.println("excellent");
+        else if (marks > 80)
+            System.out.println("good");
+        else if (marks > 70)
+            System.out.println("fair");
+        else if (marks > 60)
+            System.out.println("meets expectations");
+        else
+            System.out.println("below par");
+    }
+}
